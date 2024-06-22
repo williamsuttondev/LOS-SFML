c30edfdf2258d16ca9dadcf8bdf14ccd3d352e5a
Barnold8 <thebarnoldbiz@gmail.com>
2024-06-22 08:24:48 +0100

    Fixed up tilesheet manager, correctly grabs subsections of incoming image

--- src/TileSheetManager.cpp
+++ src/TileSheetManager.cpp
@@ -5,11 +5,12 @@
 #include <iostream>
 #include "TileSheetManager.h"
 
+void printXY(int x, int y){std::cout << "x: " << x << " | y: "<< y << std::endl;}
+
 
 std::vector<sf::Texture> TileSheetManager::generateTileMapFromImage(sf::Image& tileSheet, int tileWidth, int tileHeight){
 
     std::vector<sf::Texture> textures;
-
 
     int x_size = tileSheet.getSize().x / tileWidth;
     int y_size = tileSheet.getSize().y / tileHeight;
@@ -22,14 +23,29 @@ std::vector<sf::Texture> TileSheetManager::generateTileMapFromImage(sf::Image& t
         std::cout << "Temporary error message, height does not divide into image height equally" << std::endl;
     }
 
+    for(int x = 0; x < tileSheet.getSize().x / x_size; x++){
 
-    int x_size = tileSheet.getSize().x / tileWidth;
-    int y_size = tileSheet.getSize().y / tileHeight;
+        for (int y = 0; y < tileSheet.getSize().y / y_size ; y++)
+        {
+            sf::Texture tex;
+            sf::Image image;
 
+            int size = 800;
+            
+            printXY(x_size*x,y*y_size);
 
+            image.create(tileSheet.getSize().x,tileSheet.getSize().y);
+            image.copy(tileSheet,0,0,sf::IntRect(x_size*x,y_size*y,x_size,y_size),false);
+
+            tex.loadFromImage(image);
+
+            textures.push_back(tex);
     
+        }
+        
 
-    
+    }
+
     return textures;
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,45 @@
 #include "RenderLoop.h"
+#include "TileSheetManager.h"
 
 #define FPS 12
 
 int main() {
-    RenderLoop renderLoop(FPS);
-    renderLoop.run();
+
+
+    sf::Image f;
+    f.loadFromFile("../res/sprite_config/sprite.png");
+
+    std::vector<sf::Texture> tex = TileSheetManager::generateTileMapFromImage(f,10,10);
+
+    sf::Sprite sprite = sf::Sprite(tex[1]);
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+ 
+        // Clear screen
+        window.clear();
+ 
+        // Draw the sprite
+        window.draw(sprite);
+ 
+
+ 
+        // Update the window
+        window.display();
+    }
+    
+
+    // RenderLoop renderLoop(FPS);
+    // renderLoop.run();
     return 0;
 }
