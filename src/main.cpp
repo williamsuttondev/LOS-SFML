b48f7d9af371273c722086b87894883974c595d4
Barnold8 <thebarnoldbiz@gmail.com>
2024-06-25 09:27:50 +0100

    Revert main file to usable state

--- src/main.cpp
+++ src/main.cpp
@@ -45,60 +45,15 @@ void cleanup(std::vector<SceneObject*>& objects) {
 }
 
 int main() {
-    // sf::Texture texture;
-    // if (!texture.loadFromFile("res/sprite_config/sprite.png")) {
-    //     std::cerr << "Error loading texture" << std::endl;
-    // }
-    // AnimatedSprite* animatedSprite = new AnimatedSprite("res/sprite_config/sprite_config.json", texture);
-    // SceneObject* sceneObject = new SceneObject(animatedSprite, true, playerControlFunction);
-    // RenderLoop renderLoop(CHARACTER_FPS);
-    // renderLoop.addObject(sceneObject);
-    // renderLoop.run();
-    // cleanup(renderLoop.getSceneObjects());
-    // return 0;
-
-    ClickableButton c = ClickableButton(40,10,"res/sprite_config/sprite.png");
-
-
-    Rectangle r = {40,10,100,100};
-
-    sf::RectangleShape rs(sf::Vector2(r.width,r.height));
-    sf::RectangleShape rs2(sf::Vector2(r.width,r.height));
-
-    rs.setPosition(r.xPos,r.yPos);
-
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
-    while (window.isOpen())
-    {
-        // Process events
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // Close window: exit
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
- 
-        // Clear screen
-        window.clear();
- 
-        sf::Vector2i position = sf::Mouse::getPosition(window);
-        // std::cout << position.x << "," << position.y << std::endl;
-
-        rs2.setPosition( position.x - r.width/2,position.y-r.height/2);
-
-        // Draw the sprite
-        window.draw(c.getSprite());
-        window.draw(rs);
-        window.draw(rs2);
-
-        if(Colliders::isRectCollision(r,Rectangle(position.x - r.width/2,position.y-r.height/2,r.width,r.height)) && sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-            std::cout << "Clicked button" << std::endl;
-        }else if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){std::cout << "Not clicked it" << std::endl;}
-
-        // Update the window
-        window.display();
+    sf::Texture texture;
+    if (!texture.loadFromFile("res/sprite_config/sprite.png")) {
+        std::cerr << "Error loading texture" << std::endl;
     }
-
+    AnimatedSprite* animatedSprite = new AnimatedSprite("res/sprite_config/sprite_config.json", texture);
+    SceneObject* sceneObject = new SceneObject(animatedSprite, true, playerControlFunction);
+    RenderLoop renderLoop(CHARACTER_FPS);
+    renderLoop.addObject(sceneObject);
+    renderLoop.run();
+    cleanup(renderLoop.getSceneObjects());
+    return 0;
 }
