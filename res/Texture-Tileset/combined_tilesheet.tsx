8a1ed41a70dcd04e5972619d9e1e435d4534d371
William Sutton <williamace@live.co.uk>
2024-06-26 23:33:34 +0100

    Added a combined tileset (following command used: montage TX\ Plant.png TX\ Player.png TX\ Props.png TX\ Shadow.png TX\ Shadow\ Plant.png TX\ Struct.png TX\ Tileset\ Grass.png TX\ Tileset\ Stone\ Ground.png TX\ Tileset\ Wall.png -tile 3x3 -geometry 512x512+0+0 combined_tilesheet.png) with ImageMagik, its not perfect but what is required for the CSV to function correcttly

---
+++ res/Texture-Tileset/combined_tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="combined_tilesheet" tilewidth="32" tileheight="32" tilecount="2304" columns="48">
+ <image source="../Texture/combined_tilesheet.png" width="1536" height="1536"/>
+</tileset>
