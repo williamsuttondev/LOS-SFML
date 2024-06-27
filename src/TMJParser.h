f31061ea097d5f85e82d40ff7268de992e82a4c4
William Sutton <williamace@live.co.uk>
2024-06-27 22:47:38 +0100

    Added TMJ Parser header

---
+++ src/TMJParser.h
@@ -0,0 +1,24 @@
+#pragma once 
+
+#include <SFML/Graphics.hpp>
+#include <json.hpp>
+#include <string>
+#include <vector>
+
+class TMJParser {
+public:
+    TMJParser(const std::string& filePath);
+    const std::vector<std::vector<sf::Image>>& getLayerImages() const;
+
+private:
+    nlohmann::json m_root;
+    std::vector<std::string> m_tilesetSources;
+    std::vector<std::vector<sf::Image>> m_layerImages;
+
+    void parseTilesets();
+    void parseLayers();
+    void parseTileLayer(const nlohmann::json& layer);
+    std::string getTilesetSource(int tileID);
+    sf::Image loadTileImage(const std::string& tilesetSource, int tileID);
+    std::string getTilesetImageSource(const std::string& tsxFilePath);
+};
